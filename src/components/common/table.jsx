87eedc13d03868bf312688835bc754cbcd48ad3b
Kayhan.Kashi@gmail.com <Kayhan.Kashi@gmail.com>
2020-11-22 09:43:18 +0330

    Before adding Routing

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
 
-const Table = (props) => {
-  const { data, sortColumn, onSort, columns } = props;
+const Table = ({ data, sortColumn, onSort, columns }) => {
   return (
     <table className="table">
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
