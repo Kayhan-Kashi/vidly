75b1c277829ba4bc23be566d9634b310f1c8c92b
Kayhan.Kashi@gmail.com <Kayhan.Kashi@gmail.com>
2020-11-23 10:55:08 +0330

    Basic validation & Input extraction

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import Input from "./common/input";
 
 class LoginForm extends Component {
   state = {
     account: { username: "", password: "" },
+    errors: {},
   };
   username = React.createRef();
 
@@ -10,8 +12,16 @@ class LoginForm extends Component {
   //   this.username.current.focus();
   // }
 
+  validate = () => {
+    return { username: "Username is required." };
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors });
+    if (errors) return;
     console.log("submitted");
   };
 
@@ -27,30 +37,18 @@ class LoginForm extends Component {
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              autoFocus
-              value={account.username}
-              onChange={this.handleChange}
-              name="username"
-              ref={this.username}
-              class="form-control"
-              id="username"
-              type="text"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              class="form-control"
-              name="password"
-              id="password"
-              value={account.password}
-              onChange={this.handleChange}
-              type="text"
-            />
-          </div>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+          />
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
